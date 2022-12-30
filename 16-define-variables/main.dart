3160bb132f42f144d30645efaa35d29c181939a0
Eng-Mohamed Elsayed <mohamed.elsayed.course@gmail.com>
2022-12-30 19:43:17 +0300

    ADD Lesson 16 define varabile

---
+++ 16-define-variables/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  // * define
+  // 1
+  int num1;
+  num1 = 10;
+  print(num1);
+
+  // 2
+  double num2, num3;
+  num2 = 12.5;
+  num3 = 20.5;
+  print('num2 = $num2 , num3 = $num3');
+
+  // 3
+  String ssn = '16494644614', name = 'mohamed';
+  print('name : $name , ssn : $ssn ');
+
+  // 4
+  double salary = 500.50;
+  salary = 1000.70; // override
+  print('salary = $salary');
+}
