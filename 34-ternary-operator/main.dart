9f79f46bbc85dbcf93f0072fb26293b5ab0f86dd
Eng-Mohamed Elsayed <mohamed.elsayed.course@gmail.com>
2023-02-16 17:25:14 +0300

    ADD Lecture  35 loop for

--- 34-ternary-operator/main.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  // * apply
-  5 < 3 ? print('Greater than') : print('less than');
-
-  // * return
-  String result = 5 > 3 ? 'Greater than' : 'Less than';
-  print('result = $result');
-}
