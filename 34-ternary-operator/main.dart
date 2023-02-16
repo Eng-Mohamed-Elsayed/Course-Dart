0b4ba9fb4cb0c737a143c08adcf9ed0df7e7ed4a
Eng-Mohamed Elsayed <mohamed.elsayed.course@gmail.com>
2023-02-16 17:23:18 +0300

    ADD Lecture 34 35

---
+++ 34-ternary-operator/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  // * apply
+  5 < 3 ? print('Greater than') : print('less than');
+
+  // * return
+  String result = 5 > 3 ? 'Greater than' : 'Less than';
+  print('result = $result');
+}
